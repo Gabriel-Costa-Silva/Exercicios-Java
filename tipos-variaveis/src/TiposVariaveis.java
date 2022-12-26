a938afa1fb1b4f9af35e0c61b27cf97b4c514826
Gabriel-Costa-Silva <gabrielcostasilva.1918@gmail.com>
2022-12-26 19:58:17 -0300

    Adcionando tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        double salario =  2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroNormal;
+
+
+
+        int numero = 10;
+        numero= 5;
+
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        //estudar tipos pirmitivos
+
+        //estudar Strings
+
+        String nome = "gabriel da costa silva";
 }
